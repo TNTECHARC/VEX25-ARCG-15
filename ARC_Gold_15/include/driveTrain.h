78bb96015f5d7bbfeff4f5a77963f8a43d9007c0
erbadger27-create <erbadger27@gmail.com>
2025-09-24 19:48:51 -0500

    driveTrain.h
    Drivetrain motor/motor groups header

---
+++ ARC_Gold_15/include/driveTrain.h
@@ -0,0 +1,17 @@
+#include "vex.h"
+
+using namespace std;
+
+void driveTrain{
+    motor LMotor1 = motor(PORT1, false)
+    motor LMotor2 = motor(PORT2, false)
+    motor LMotor3 = motor(PORT3, false)
+    motor LMotor4 = motor(PORT4, false)
+    motor_group lDrive = motor_group(LMotor1, LMotor2, LMotor3, LMotor4)
+
+    motor RMotor1 = motor(PORT5, false)
+    motor RMotor2 = motor(PORT6, false)
+    motor RMotor3 = motor(PORT7, false)
+    motor RMotor4 = motor(PORT8, false)
+    motor_group rDrive = motor_group(RMotor1, RMotor2, RMotor3, RMotor4)
+}
