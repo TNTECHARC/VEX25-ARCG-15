b987f986e29265c5ba3071310a70d52488ba1598
Cobyashi <144290613+Cobyashi@users.noreply.github.com>
2025-09-25 10:35:23 -0500

    Updated driveTrain.h and driveTrain.cpp syntax

--- ARC_Gold_15/include/vex.h
+++ ARC_Gold_15/include/vex.h
@@ -6,6 +6,8 @@
 #include "v5.h"
 #include "v5_vcs.h"
 
+#include "driveTrain.h"
+
 
 #define waitUntil(condition)                                                   \
   do {                                                                         \
--- ARC_Gold_15/src/Drivetrain.cpp
+++ ARC_Gold_15/src/Drivetrain.cpp
@@ -1,23 +1,18 @@
 #include "vex.h"
 #include "driveTrain.h"
 
-using namespace std;
 
-class Drive {
-private:
-public:
-    // drives robot forward and back
-    void drive() {
-        if(controler.axis2 > 5 || controler.axis2 < -5){
-            lDrive.spin(forward, controler.axis2, rpm);
-            rDrive.spin(forward, controler.axis2, rpm);  
-        }
+// drives robot forward and back
+void driveTrain::drive() {
+    if(controller1.Axis2.value() > 5 || controller1.Axis2.value() < -5){
+        lDrive.spin(forward, controller1.Axis2.value(), rpm);
+        rDrive.spin(forward, controller1.Axis2.value(), rpm);  
     }
-    // turns robot L & R
-    void turn() {
-        if(controler.axis1 > 5 || controler.axis1 < -5){
-            lDrive.spin(forward, controler.axis1, rpm);
-            rDrive.spin(backward, controler.axis1, rpm);
-        }
-    }  
-};
+}
+// turns robot L & R
+void driveTrain::turn() {
+    if(controller1.Axis1.value() > 5 || controller1.Axis1.value() < -5){
+        lDrive.spin(forward, controller1.Axis1.value(), rpm);
+        rDrive.spin(reverse, controller1.Axis1.value(), rpm);
+    }
+}  
