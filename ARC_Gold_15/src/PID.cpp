93af021ec3ef45c22a0e69f2c6af823475d68c6b
Emelia <egbelicek42@tntech.edu>
2025-09-26 19:46:02 -0500

    Added a settle function :)

--- ARC_Gold_15/src/PID.cpp
+++ ARC_Gold_15/src/PID.cpp
@@ -6,10 +6,13 @@ using namespace vex;
 class PID {
 
 private:
-    float Kp, Ki, Kd, Integral, Derivative, previousError; //PID Constants
+    float Kp, Ki, Kd, Integral, Derivative, previousError; //PID Constants 
+    float settleError = .5; //distance from point that we want the bot to stop (half an inch)
+    float timeSpentsettled = 0;
 
 public:
-    PID () { //Default Constructor
+    //Default Constructor
+    PID () { 
         Kp = 0.00;
         Ki = 0.00;
         Kd = 0.00;
@@ -17,6 +20,7 @@ public:
         Derivative = 0.00;
          
     }
+    //Defines the parts of PID for the function
     PID (float kP, float Ki, float Kd, float Integral) {
         this-> Kp = Kp;
         this-> Ki = Ki;
@@ -48,6 +52,10 @@ public:
         this-> Kd = Kd;
     }
 
+    
+    /// @brief This is the function that calculates PID 
+    /// @param error 
+    /// @return 
     float calculatePID(float error) {
         float PID;
 
@@ -55,8 +63,26 @@ public:
         Derivative = error - previousError;
         PID = (Kp*error) + (Ki*Integral) + (Kd*Derivative);
         previousError = error;
+        if (error < settleError)
+        {
+            timeSpentsettled ++;
+        }
+        else 
+            timeSpentsettled = 0;
 
         return PID;
+    }
+
+    /// @brief If it spends more than 3 secs settling it'll stop
+    /// @return 
+    bool isSettled(){    
+        if(timeSpentsettled > 3000)
+        {
+            return true
+        }
+        else 
+        return false
+
     }
 
 };
